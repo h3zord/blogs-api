8295fbc1c181a88c89bd88a3629042a9a32ced06
Lucas <lucaschmain@gmail.com>
2023-03-01 23:08:35 -0300

    config railway deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,25 +1,20 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || 'test';
-
-const suffix = {
-  dev: '-dev',
-  development: '-dev',
-  test: '-test',
-};
+const { PGHOST, PGPORT, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
 const options = {
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DATABASE || 'blogs-api'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
-  dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
+  host: PGHOST,
+  port: PGPORT,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  dialect: 'postgres',
+  define: {
+    timestamps: true,
   },
-  logging: process.env.DEBUG !== 'false',
+  dialectOptions: {
+    schema: 'public',
+  },
 };
 
 module.exports = {
